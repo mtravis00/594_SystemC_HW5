d706696730d0e7bff41d8010d3af4512cce9ef8c
Mattew <mjtravis@gmail.com>
2017-03-22 23:47:49 -0500

    still broken

--- 594_DM_HW5_Multiply/busPad.cpp
+++ 594_DM_HW5_Multiply/busPad.cpp
@@ -5,11 +5,19 @@ void busPad::padding() {
 //	sc_uint<9> ires;
 	int stop;
 
-	//sc_lv<8> var;
+	sc_lv<4> pad = (sc_lv<4>) (SC_LOGIC_0);
+	cout << "pad value " << pad << endl;
 
-//	var = (sc_lv<4>)ain;
-	//cout << "var " << var << endl;
-	padded_out = (sc_lv<4>)ain;
+	sc_lv<8> out;
+	sc_lv<4> in;
+
+	in = ain.read();
+	cout << "input  " << in << endl;
+
+	out = (pad, in);
+	cout << "output " << out << endl;
+
+	padded_out = (sc_lv<8>)out;
 	cout << "padded_out " << padded_out << endl;
 	cin >> stop;
 
